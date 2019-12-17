e9ccb3af2ccdf5492ab376ffdb759ef91566f92d
Ajay Verma <48929433+ajayv322@users.noreply.github.com>
2019-12-17 21:51:49 +0530

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,24 @@
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+
+
+def load_image(filename):
+	img = load_img(filename, target_size=(224, 224))
+	img = img_to_array(img)
+	img = img.reshape(1, 224, 224, 3)
+	img = img.astype('float32')
+	img = img - [123.68, 116.779, 103.939]
+	return img
+
+# load an image and predict the class
+def run():
+	# load the image
+	img = load_image('sample_image.jpg')
+	# load model
+	model = load_model('final_model.h5')
+	# predict the class
+	result = model.predict(img)
+	print(result[0])
+
+run()
